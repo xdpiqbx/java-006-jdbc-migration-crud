61ecad143688b2afe789e34d4dd46468fe9200ee
xdpiqbx <xdpiqbx@gmail.com>
2022-12-14 00:53:46 +0200

    cw mod 4 lesson 10 jdbc done

--- app/src/main/java/cw/migcrud/feature/human/HumanDaoService.java
+++ app/src/main/java/cw/migcrud/feature/human/HumanDaoService.java
@@ -1,13 +1,13 @@
 package cw.migcrud.feature.human;
 
-import lombok.RequiredArgsConstructor;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class HumanDaoService {
 // @RequiredArgsConstructor
@@ -17,6 +17,12 @@ public class HumanDaoService {
     private PreparedStatement createSt;
     private PreparedStatement getByIdSt;
     private PreparedStatement selectMaxIdSt;
+    private PreparedStatement getAllSt;
+    private PreparedStatement updateSt;
+    private PreparedStatement deleteByIdSt;
+    private PreparedStatement existsByIdSt;
+    private PreparedStatement clearSt;
+    private PreparedStatement searchSt;
 
     public HumanDaoService(Connection connection) throws SQLException {
         this.createSt = connection.prepareStatement(
@@ -27,6 +33,24 @@ public class HumanDaoService {
         );
         this.selectMaxIdSt = connection.prepareStatement(
             "SELECT MAX(id) AS maxId FROM human"
+        );
+        this.getAllSt = connection.prepareStatement(
+            "SELECT id, name, birthday, gender FROM human"
+        );
+        this.updateSt = connection.prepareStatement(
+            "UPDATE human SET name = ?, birthday = ?, gender = ? WHERE id = ?"
+        );
+        this.deleteByIdSt = connection.prepareStatement(
+            "DELETE FROM human WHERE id = ?"
+        );
+        this.existsByIdSt = connection.prepareStatement(
+            "SELECT COUNT(*) > 0 as humanExists FROM human WHERE id = ?"
+        );
+        this.clearSt = connection.prepareStatement(
+            "DELETE FROM human"
+        );
+        this.searchSt = connection.prepareStatement(
+            "SELECT id, name, birthday, gender FROM human WHERE name LIKE ?"
         );
     }
 
@@ -67,8 +91,67 @@ public class HumanDaoService {
             return result;
         }
     }
+    public List<Human> getAll() throws SQLException {
+        return getHumans(getAllSt);
+    }
+    public void update(Human human) throws SQLException {
+        // "UPDATE human SET name = ?, birthday = ?, gender = ? WHERE id = ?"
+        updateSt.setString(1, human.getName());
+        updateSt.setString(2, human.getBirthday().toString());
+        updateSt.setString(3, human.getGender().name());
+        updateSt.setLong(4, human.getId());
 
-    public List<Human> getAll(){
-        return null;
+        updateSt.executeUpdate();
+    }
+
+    public void deleteById(long id) throws SQLException {
+        deleteByIdSt.setLong(1, id);
+        deleteByIdSt.executeUpdate();
+    }
+
+    public boolean exists(long id) throws SQLException {
+        existsByIdSt.setLong(1, id);
+        try(ResultSet rs = existsByIdSt.executeQuery()){
+            rs.next();
+            return rs.getBoolean("humanExists");
+        }
+    }
+
+    public long save(Human human) throws SQLException {
+        if(exists(human.getId())){
+            update(human);
+            return human.getId();
+        }
+        return create(human);
+    }
+
+    public void clear() throws SQLException {
+        clearSt.executeUpdate();
+    }
+
+    public List<Human> searchByName(String query) throws SQLException {
+        searchSt.setString(1, "%"+query+"%");
+        return getHumans(searchSt);
+    }
+
+    private List<Human> getHumans(PreparedStatement searchSt) throws SQLException {
+        try(ResultSet rs = searchSt.executeQuery()){
+            List<Human> result = new ArrayList<>();
+            while(rs.next()){
+                Human human = new Human();
+                human.setId(rs.getLong("id"));
+                human.setName(rs.getString("name"));
+                String birthday = rs.getString("birthday");
+                if(birthday != null){
+                    human.setBirthday(LocalDate.parse(birthday));
+                }
+                String gender = rs.getString("gender");
+                if(gender != null){
+                    human.setGender(Human.Gender.valueOf(gender));
+                }
+                result.add(human);
+            }
+            return result;
+        }
     }
 }
--- app/src/test/java/cw/migcrud/feature/human/HumanDaoServiceTest.java
+++ app/src/test/java/cw/migcrud/feature/human/HumanDaoServiceTest.java
@@ -11,7 +11,6 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -25,6 +24,7 @@ class HumanDaoServiceTest {
         DatabaseInitService.initDb(connectionUrl);
         connection = DriverManager.getConnection(connectionUrl);
         daoService = new HumanDaoService(connection);
+        daoService.clear();
     }
     @AfterEach
     public void afterEach() throws SQLException {
@@ -65,7 +65,6 @@ class HumanDaoServiceTest {
             Assertions.assertEquals(originalHuman.getBirthday(), saved.getBirthday());
             Assertions.assertEquals(originalHuman.getGender(), saved.getGender());
         }
-
     }
 
     @Test
@@ -81,6 +80,107 @@ class HumanDaoServiceTest {
         List<Human> expectedHumans = Collections.singletonList(expected);
         List<Human> actualHumans = daoService.getAll();
 
-        Assertions.assertEquals();
+        Assertions.assertEquals(expectedHumans, actualHumans);
+    }
+
+    @Test
+    void update() throws SQLException {
+        Human original = new Human();
+        original.setName("TestName");
+        original.setBirthday(LocalDate.now());
+        original.setGender(Human.Gender.male);
+
+        long id = daoService.create(original);
+        original.setId(id);
+
+        original.setName("New Name");
+        original.setBirthday(LocalDate.now().plusDays(1));
+        original.setGender(Human.Gender.female);
+        daoService.update(original);
+
+        Human updated = daoService.getById(id);
+        Assertions.assertEquals(id, updated.getId());
+        Assertions.assertEquals("New Name", updated.getName());
+        Assertions.assertEquals(LocalDate.now().plusDays(1), updated.getBirthday());
+        Assertions.assertEquals(Human.Gender.female, updated.getGender());
+    }
+
+    @Test
+    void deleteById() throws SQLException {
+        Human original = new Human();
+        original.setName("TestName");
+        original.setBirthday(LocalDate.now());
+        original.setGender(Human.Gender.male);
+
+        long id = daoService.create(original);
+        original.setId(id);
+
+        daoService.deleteById(id);
+
+        Assertions.assertNull(daoService.getById(id));
+    }
+
+    @Test
+    void exists() throws SQLException {
+        Human original = new Human();
+        original.setName("TestName");
+        original.setBirthday(LocalDate.now());
+        original.setGender(Human.Gender.male);
+
+        long id = daoService.create(original);
+        original.setId(id);
+
+        Assertions.assertTrue(daoService.exists(id));
+        Assertions.assertFalse(daoService.exists(-1));
+    }
+
+    @Test
+    void save() throws SQLException {
+        Human newHuman = new Human();
+        newHuman.setName("TestName");
+        newHuman.setBirthday(LocalDate.now());
+        newHuman.setGender(Human.Gender.male);
+
+        long id = daoService.save(newHuman);
+
+        Assertions.assertTrue(daoService.exists(id));
+    }
+
+    @Test
+    public void saveOnExistingUser() throws SQLException{
+        Human newHuman = new Human();
+        newHuman.setName("TestName");
+        newHuman.setBirthday(LocalDate.now());
+        newHuman.setGender(Human.Gender.male);
+
+        long id = daoService.save(newHuman);
+        newHuman.setId(id);
+
+        newHuman.setName("New Name");
+        daoService.save(newHuman);
+
+        Human updated = daoService.getById(id);
+        Assertions.assertEquals("New Name", updated.getName());
+    }
+
+    @Test
+    void searchOnEmpty() throws SQLException {
+        Assertions.assertEquals(
+            Collections.emptyList(),
+            daoService.searchByName("name")
+        );
+    }
+    @Test
+    void searchOnFilledDB() throws SQLException {
+        Human newHuman = new Human();
+        newHuman.setName("TestName");
+        newHuman.setBirthday(LocalDate.now());
+        newHuman.setGender(Human.Gender.male);
+
+        long id = daoService.save(newHuman);
+
+        List<Human> actual = daoService.searchByName("Test");
+        Assertions.assertEquals(1, actual.size());
+        Assertions.assertEquals(id, actual.get(0).getId());
     }
 }
